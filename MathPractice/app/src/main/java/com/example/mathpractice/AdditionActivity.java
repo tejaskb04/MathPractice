9f941411d76b852b18b7cc8ea921aba1af82f389
tejaskb04 <tejaskb@gmail.com>
2017-09-07 19:46:48 -0700

    Addition Code

--- MathPractice/app/src/main/java/com/example/mathpractice/AdditionActivity.java
+++ MathPractice/app/src/main/java/com/example/mathpractice/AdditionActivity.java
@@ -9,16 +9,40 @@ import android.widget.Button;
 public class AdditionActivity extends AppCompatActivity {
 
     private Button lvlOneBtn;
+    private Button lvlTwoBtn;
+    private Button lvlThreeBtn;
+    private Button lvlFourBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addition);
         lvlOneBtn = (Button) findViewById(R.id.lvl_1);
+        lvlTwoBtn = (Button) findViewById(R.id.lvl_2);
+        lvlThreeBtn = (Button) findViewById(R.id.lvl_3);
+        lvlFourBtn = (Button) findViewById(R.id.lvl_4);
         lvlOneBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(AdditionActivity.this, AdditionLevelOneActivity.class));
+            }
+        });
+        lvlTwoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdditionActivity.this, AdditionLevelTwoActivity.class));
+            }
+        });
+        lvlThreeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdditionActivity.this, AdditionLevelThreeActivity.class));
+            }
+        });
+        lvlFourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdditionActivity.this, AdditionLevelFourActivity.class));
             }
         });
     }
