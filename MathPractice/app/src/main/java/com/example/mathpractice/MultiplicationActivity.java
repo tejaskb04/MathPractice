4f5de663fb3dc7374ee963bc5a34ed160b1878fd
tejaskb04 <tejaskb@gmail.com>
2017-09-09 15:46:02 -0700

    Multiplication Works

--- MathPractice/app/src/main/java/com/example/mathpractice/MultiplicationActivity.java
+++ MathPractice/app/src/main/java/com/example/mathpractice/MultiplicationActivity.java
@@ -1,13 +1,49 @@
 package com.example.mathpractice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MultiplicationActivity extends AppCompatActivity {
+
+    private Button lvlOneBtn;
+    private Button lvlTwoBtn;
+    private Button lvlThreeBtn;
+    private Button lvlFourBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_multiplication);
+        lvlOneBtn = (Button) findViewById(R.id.lvl_1);
+        lvlTwoBtn = (Button) findViewById(R.id.lvl_2);
+        lvlThreeBtn = (Button) findViewById(R.id.lvl_3);
+        lvlFourBtn = (Button) findViewById(R.id.lvl_4);
+        lvlOneBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MultiplicationActivity.this, MultiplicationLevelOneActivity.class));
+            }
+        });
+        lvlTwoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MultiplicationActivity.this, MultiplicationLevelTwoActivity.class));
+            }
+        });
+        lvlThreeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MultiplicationActivity.this, MultiplicationLevelThreeActivity.class));
+            }
+        });
+        lvlFourBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MultiplicationActivity.this, MultiplicationLevelFourActivity.class));
+            }
+        });
     }
 }
